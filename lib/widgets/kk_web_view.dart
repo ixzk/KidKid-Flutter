47eac27b84a65f785f0be6e60d2c7356fefd53f0
ixzk <1070794219@qq.com>
2019-11-04 23:55:24 +0800

    [模块] webView封装

---
+++ lib/widgets/kk_web_view.dart
@@ -0,0 +1,31 @@
+// 网页浏览器
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:kidkid/util/global_colors.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class KKWebView extends StatelessWidget {
+  
+  final String title;
+  final String url;
+
+  KKWebView({this.title, this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: GlobalColors.bgColor,
+      navigationBar: CupertinoNavigationBar(
+        border: null,
+        backgroundColor: GlobalColors.white,
+        actionsForegroundColor: GlobalColors.red,
+        middle: Text('益智游戏')
+      ),
+      child: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      )
+    );
+  }
+}
