112d7df9b289be59f9c5e6d684afb68b24c1446a
ixzk <1070794219@qq.com>
2019-11-04 23:49:23 +0800

    [模块]核心页面架构搭建

--- lib/app.dart
+++ lib/app.dart
@@ -1,76 +1,110 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kidkid/pages/ai/ai.dart';
+import 'package:kidkid/pages/video/video.dart';
 import 'package:kidkid/util/global_colors.dart';
+import 'package:kidkid/pages/main_page.dart';
+import 'package:kidkid/pages/music/music.dart';
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   home: Material(
-    //     child: Player(),
-    //   ),
-    // );
     return MaterialApp(
       home: CupertinoTabScaffold(
         tabBar: CupertinoTabBar(
           border: null,
           backgroundColor: GlobalColors.white,
           items: [
-            BottomNavigationBarItem(
-              icon: Image.asset('images/tabbar/music.png', width: 25.0, height: 25.0),
-              activeIcon: Image.asset('images/tabbar/music_s.png', width: 25.0, height: 25.0),
-              title: Text('音乐')
+            _getBottomNavigationBarItem(
+              icon: Image.asset('images/i1.png'),
+              activeIcon: Image.asset('images/i1_s.png')
             ),
-            BottomNavigationBarItem(
-              icon: Image.asset('images/tabbar/discover.png', width: 25.0, height: 25.0),
-              activeIcon: Image.asset('images/tabbar/discover_s.png', width: 25.0, height: 25.0),
-              title: Text('发现')
+            _getBottomNavigationBarItem(
+              icon: Image.asset('images/voice.png'),
+              activeIcon: Image.asset('images/ai.png')
             ),
-            
+            _getBottomNavigationBarItem(
+              icon: Image.asset('images/i2.png'),
+              activeIcon: Image.asset('images/i2_s.png')
+            )
           ],
           iconSize: 25.0,
           activeColor: Color(0xFFFC1F50),
         ),
         tabBuilder: (context, index) {
-          return Container(
-            color: GlobalColors.bgColor,
-          );
-          // Widget page;
-          // switch (index) {
-          //   case 0: page = MainPage(title: '音乐', body: Music()); break;
-          //   case 1: page = MainPage(title: '歌单', body: Channel()); break;
-          //   case 2: page = MainPage(title: '社区', body: Square()); break;
-          //   case 3: page = MainPage(title: '我的', body: Mine()); break;
-          // }
-
           // return Container(
           //   color: GlobalColors.bgColor,
-          //   child: Column(
-          //     children: <Widget>[
-          //       Expanded(
-          //         child: page,
-          //       ),
-          //       MiniPlayer()
-          //     ],
-          //   ),
           // );
+          Widget page;
+          switch (index) {
+            case 0: page = MainPage(title: '广场', body: Music()); break;
+            case 1: page = AI(); break;
+            case 2: page = MainPage(title: '视频', body: Video()); break;
+          }
+
+          return Container(
+            color: GlobalColors.bgColor,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: page,
+                ),
+                // MiniPlayer()
+                index == 1 ? Container(
+                  height: 50.0,
+                  color: Colors.green,
+                ): Container()
+              ],
+            ),
+          );
         },
       )
     );
   }
 
-  BottomNavigationBarItem _getBottomNavigationBarItem(String name, {Widget icon, Widget activeIcon}) {
+  BottomNavigationBarItem _getBottomNavigationBarItem({Widget icon, Widget activeIcon}) {
     return BottomNavigationBarItem(
-      icon: Container(
-        width: 25.0,
-        height: 25.0,
-        child: icon,
+      icon: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 35.0,
+            height: 35.0,
+            child: icon
+          ),
+          ClipOval(
+            child: Container(
+              width: 5.0,
+              height: 5.0,
+              color: GlobalColors.white,
+            )
+          )
+        ],
       ),
-      activeIcon: Container(
-        width: 25.0,
-        height: 25.0,
-        child: icon,
+      activeIcon: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 35.0,
+            height: 35.0,
+            child: activeIcon
+          ),
+          ClipOval(
+            child: Container(
+              width: 5.0,
+              height: 5.0,
+              color: GlobalColors.red,
+            )
+          )
+        ],
       ),
-      title: Text(name)
+      // title: Text(name)
     );
   }
+
+  // AIBot 动画效果
+  // Widget aibotTransition() {
+  //   return 
+  // }
 }
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,31 @@
+// Tabbar选项卡使用的结构
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kidkid/util/global_colors.dart';
+import 'package:kidkid/widgets/title_line.dart';
+
+class MainPage extends StatelessWidget {
+
+  final String title;
+  final Widget body;
+  final List<ChangeNotifier> providers;
+
+  MainPage({this.title, this.body, this.providers});
+
+  Widget build(BuildContext context) {
+
+    return Material(
+      child: CupertinoPageScaffold(
+        backgroundColor: GlobalColors.bgColor,
+        navigationBar: CupertinoNavigationBar(
+          border: null,
+          backgroundColor: GlobalColors.bgColor,
+          leading: TitleLine(title: '${title ?? ""}'),
+          automaticallyImplyMiddle: false,
+        ),
+        child: body
+      ),
+    );
+  }
+}
