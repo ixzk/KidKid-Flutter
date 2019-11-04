ad177191d778e98a116005dd89698ae876775ef9
ixzk <1070794219@qq.com>
2019-11-04 23:55:01 +0800

    [模块] 大标题、更多标题组件

---
+++ lib/widgets/title_line.dart
@@ -0,0 +1,43 @@
+// 带下划线的标题
+
+import 'package:flutter/material.dart';
+import 'package:kidkid/util/global_colors.dart';
+
+class TitleLine extends StatelessWidget {
+
+  final String title;
+
+  TitleLine({ this.title });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          bottom: 10,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(5.0),
+            child: Container(
+              width: this.title.length * 25.0,
+              height: 10,
+              color: GlobalColors.red,
+            ),
+          )
+        ),
+        Positioned(
+          width: this.title.length * 25.0,
+          bottom: 10,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                this.title ?? '', 
+                style: TextStyle(color: GlobalColors.black, letterSpacing: 5.0, fontSize: 20.0, fontWeight: FontWeight.bold)
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/widgets/title_more.dart
@@ -0,0 +1,50 @@
+// 带更多按钮的标题
+
+import 'package:flutter/material.dart';
+import 'package:kidkid/util/global_colors.dart';
+
+class TitleMore extends StatelessWidget {
+
+  final String title;
+  final Function pressMore;
+
+  TitleMore({this.title, this.pressMore});
+
+  @override
+  Widget build(BuildContext context) {
+
+    var row = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          title ?? '', 
+          style: TextStyle(
+            color: GlobalColors.black,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500
+          )
+        )
+      ],
+    );
+
+    if (pressMore != null) {
+      row.children.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              '更多', 
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14.0
+              )
+            ),
+            Icon(Icons.chevron_right, color: Colors.grey)
+          ],
+        )
+      );
+    }
+
+    return row;
+  }
+}
