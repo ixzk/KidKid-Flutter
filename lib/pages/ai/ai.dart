4e51e8d96ef8d448d9e1482ee27da6e33fdf23b3
ixzk <1070794219@qq.com>
2019-11-04 23:56:14 +0800

    [模块] AI助手基本模块

---
+++ lib/pages/ai/ai.dart
@@ -0,0 +1,34 @@
+// 语音助手功能
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:kidkid/util/global_colors.dart';
+import 'package:kidkid/widgets/title_line.dart';
+
+class AI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: CupertinoPageScaffold(
+        backgroundColor: GlobalColors.bgColor,
+        navigationBar: CupertinoNavigationBar(
+          border: null,
+          backgroundColor: GlobalColors.bgColor,
+          middle: TitleLine(title: '小K'),
+          // leading: TitleLine(title: '${title ?? ""}'),
+        ),
+        child: Container(
+          // child: ListView.builder(
+          //   itemCount: 10,
+          //   itemBuilder: (context, index) {
+          //     if (index == 0) {
+          //       return TitleLine(title: '你好，我是小K');
+          //     } else {
+          //       return TitleLine(title: '你好，我是小K');
+          //     }
+          //   },
+          // ),
+        )
+      ),
+    );
+  }
+}
